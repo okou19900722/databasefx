dd54cf159b3eac0adc4e43f73e90104e9c52b79c
yangkui <752544765@qq.com>
2020-04-15 16:58:13 +0800

    modify README.adoc

---
+++ app/src/main/java/com/openjfx/database/app/controls/DataLabel.java
@@ -0,0 +1,37 @@
+package com.openjfx.database.app.controls;
+
+import com.openjfx.database.app.skin.DataLabelSkin;
+import javafx.scene.control.Control;
+import javafx.scene.control.Skin;
+
+/**
+ *
+ * 自定义数据label显示组件
+ * @author yangkui
+ * @since 1.0
+ *
+ */
+public class DataLabel extends Control {
+
+    private String text;
+
+    public DataLabel(String text) {
+        this.text = text;
+    }
+
+    public DataLabel() {
+    }
+
+    @Override
+    protected Skin<?> createDefaultSkin(){
+        return new DataLabelSkin(this);
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
