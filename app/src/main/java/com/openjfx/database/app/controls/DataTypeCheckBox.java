65937d43a14ad459bb271f744ef45d9bbc429895
yangkui <752544765@qq.com>
2020-05-19 14:32:00 +0800

    fix bug

--- app/src/main/java/com/openjfx/database/app/controls/DataTypeCheckBox.java
+++ app/src/main/java/com/openjfx/database/app/controls/DataTypeCheckBox.java
@@ -3,14 +3,17 @@ package com.openjfx.database.app.controls;
 import com.openjfx.database.DataCharset;
 
 import com.openjfx.database.model.DatabaseCharsetModel;
-import javafx.scene.control.ChoiceBox;
+import org.controlsfx.control.PrefixSelectionChoiceBox;
 
-public class DataTypeCheckBox extends ChoiceBox<DatabaseCharsetModel> {
+public class DataTypeCheckBox extends PrefixSelectionChoiceBox<DatabaseCharsetModel> {
 
     private final DataCharset charset;
 
     public DataTypeCheckBox(DataCharset charset) {
         this.charset = charset;
         this.getItems().addAll(charset.getDatabaseCharset());
+        setOnScrollStarted(event -> {
+            System.out.println(event.getDeltaX());
+        });
     }
 }
--- app/src/main/java/com/openjfx/database/app/controls/DesignTableView.java
+++ app/src/main/java/com/openjfx/database/app/controls/DesignTableView.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Skin;
 import javafx.scene.control.TableView;
 
 public class DesignTableView<T> extends TableView<T> {
+
     @Override
     protected Skin<?> createDefaultSkin() {
         return new TableDataViewSkin(this);
