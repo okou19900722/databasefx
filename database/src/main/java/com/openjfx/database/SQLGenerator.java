995a8f41bd5fc820793b05d798185f094f6c6d34
yangkui <752544765@qq.com>
2020-05-23 15:30:39 +0800

    Remove useless code

--- database/src/main/java/com/openjfx/database/SQLGenerator.java
+++ database/src/main/java/com/openjfx/database/SQLGenerator.java
@@ -1,53 +1,13 @@
 package com.openjfx.database;
 
-import com.openjfx.database.model.TableColumnMeta;
-import io.vertx.core.json.JsonObject;
-
-import java.util.List;
-import java.util.concurrent.Future;
 
 /**
- * sql生成接口
+ * SQL statement generation interface
  *
  * @author yangkui
  * @since 1.0
  */
 public interface SQLGenerator {
-    /**
-     * SELECT
-     *
-     * @param metas
-     * @param tableName
-     * @return
-     */
-    String select(List<TableColumnMeta> metas, String tableName);
-
-    /**
-     * UPDATE
-     *
-     * @param metas
-     * @param tableName
-     * @return
-     */
-    String update(List<TableColumnMeta> metas, String tableName);
-
-    /**
-     * INSERT
-     *
-     * @param metas
-     * @param tableName
-     * @return
-     */
-    String insert(List<TableColumnMeta> metas, String tableName);
-
-    /**
-     * DELETE
-     *
-     * @param metas
-     * @param tableName
-     * @return
-     */
-    String delete(List<TableColumnMeta> metas, String tableName);
 
     /**
      * current database create scheme
